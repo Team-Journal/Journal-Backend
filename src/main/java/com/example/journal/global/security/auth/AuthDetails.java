00598731d0dd95b4fa61f3e8906d99d488a06081
bangsil <subenmoom50@gmail.com>
2023-04-15 12:54:03 +0900

    ♻️:: RefreshToken 재발급 기능 리팩토링

--- src/main/java/com/example/journal/global/security/auth/AuthDetails.java
+++ src/main/java/com/example/journal/global/security/auth/AuthDetails.java
@@ -31,7 +31,7 @@ public class AuthDetails implements UserDetails {
 
     @Override
     public String getUsername(){
-        return user.getEmail();
+        return user.getAccountId();
     }
 
     @Override
